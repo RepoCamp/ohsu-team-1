e45df67442333abf146f1648d72f95afb22ead56
Hui Zhang <hui.zhang@oregonstate.edu>
2018-04-25 15:22:30 -0700

    Add test for adding photographer metadata field

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,9 +1,15 @@
-# Generated via
-#  `rails generate hyrax:work Image`
 require 'rails_helper'
 
 RSpec.describe Image do
-  it "has tests" do
-    skip "Add your tests here"
+  describe '#photographer' do
+    it 'has no value when created' do
+      image = Image.new
+      expect(image.photographer).to be_empty
+    end
+    it 'can set value' do
+      image = Image.new
+      image.photographer = ['Don John']
+      expect(image.photographer).to eq ['Don John']
+    end
   end
 end
